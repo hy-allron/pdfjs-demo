67df04e9f465868e21d36f94d71aece5a6cc094f
allron <hyshizhe@163.com>
2021-10-22 12:00:06 +0800

    Initial commit

---
+++ src/main/java/com/allron/pdfjsdemo/PdfjsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.allron.pdfjsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfjsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfjsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allron/pdfjsdemo/PdfjsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allron.pdfjsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfjsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
